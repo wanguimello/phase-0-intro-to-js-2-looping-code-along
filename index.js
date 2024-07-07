d400dc74420ea1eaa851acb11caac1360ba15de0
joycewangui <joycewangui590@gmail.com>
2024-07-07 12:45:33 +0700

    Adds index.js

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Code your solutions in this file
+function writeCards(names, event) {
+    const messages = [];
+    for (let i = 0; i < names.length; i++) {
+      const message = `Thank you, ${names[i]}, for the wonderful ${event} gift!`;
+      messages.push(message);
+    }
+    return messages;
+}
+
+function countDown(number) {
+    while (number >= 0) {
+      console.log(number);
+      number--;
+    }
+}
